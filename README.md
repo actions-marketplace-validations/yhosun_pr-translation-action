ccd7f31cef58fdca8610cf87452c74e1e9860549
Hosun Yoo <hosun@teamblind.com>
2022-06-28 05:30:29 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# PR Translation Action
+
+This action translates a PR (Pull Request) title, a description, and comments between two languages.
+For example, you can assign `en` to the action inputs `language-1` and `ko` to `language-2`.
+A translated Korean comment is added to an English comment, and vice versa.
+
+## Supported Events and Types
+| Event         | Types       |
+| -----------   | ----------- |
+| pull_request  | opened      |
+| issue_comment | created     |
+
+**"edited" types are not supported.**
+
+## Action Example
+```yml
+name: PR Translation
+on:
+  pull_request:
+    types: [ opened ]
+  issue_comment:
+    types: [created ]
+jobs:
+  translate:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Translate
+        uses: yhosun/pr-translation-action
+        with:
+          language-1: 'en'
+          language-2: 'ko'
+          repo-token: ${{ secrets.GITHUB_TOKEN }}
+          google-project-id: 'your-google-project-id'
+          google-credentials: ${{ secrets.GOOGLE_CREDENTIALS }}
+```
+
+## google-credentials input
+To use Google Translate API, Google credentials in JSON string format must pass to the `google-credentials` input.
+
+## Development
+It is based on node 16.  Please, check the `.nvmrc` file.
+
+### Install NCC
+```shell
+npm i -g @vercel/ncc
+```
+
+### Build a distribution version
+`npm run dev` (ncc build index.js -o dist -w)
+
+or
+
+`npm run build` (ncc build index.js -o dist)
---
+++ index.js
@@ -0,0 +1,93 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const {Translate} = require('@google-cloud/translate').v2;
+
+const octokit = github.getOctokit(core.getInput('repo-token'));
+
+const translate = new Translate({
+    projectId: core.getInput('google-project-id'),
+    credentials: JSON.parse(core.getInput('google-credentials')),
+});
+
+const TL_ICON = core.getInput('translation-emoji');
+const TL_LABEL = `${TL_ICON} *Translation*`;
+
+const main = async () => {
+    const payload = JSON.stringify(github, undefined, 2);
+    core.debug(`${payload}`);
+
+    const lang1 = core.getInput('language-1'); // e.g. en
+    const lang2 = core.getInput('language-2'); // e.g. ko
+
+    if (github.context.eventName === 'issue_comment') {
+        await translateComment(octokit, github.context.payload, lang1, lang2);
+    } else if (github.context.eventName === 'pull_request') {
+        await translatePullRequest(octokit, github.context.payload, lang1, lang2);
+    } else {
+        core.setFailed(`Unsupported trigger event: ${github.context.eventName}`);
+    }
+}
+
+const translatePullRequest = async (octokit, payload, lang1, lang2) => {
+    const title = payload.pull_request.title;
+    const desc = payload.pull_request.body;
+    const tanslatedTitle = await translateText(title, lang1, lang2);
+    const tanslatedDesc = await translateText(desc, lang1, lang2);
+
+    await octokit.rest.pulls.update({
+        owner: payload.repository.owner.login,
+        repo: payload.repository.name,
+        pull_number: payload.pull_request.number,
+        title: `${title} || ${tanslatedTitle}`,
+        body: formatDesc(desc, tanslatedDesc)
+    });
+}
+
+const translateComment = async (octokit, payload, lang1, lang2) => {
+    const comment = payload.comment.body;
+
+    if (!comment) {
+        return;
+    }
+
+    const translation = await translateText(comment, lang1, lang2);
+
+    await octokit.rest.issues.updateComment({
+        owner: payload.repository.owner.login,
+        repo: payload.repository.name,
+        comment_id: payload.comment.id,
+        body: formatDesc(comment, translation)
+    });
+
+}
+
+const translateText = async (text, lang1, lang2) => {
+    if (!text) {
+        core.debug('empty text for translation');
+        return '';
+    }
+
+    let [detections] = await translate.detect(text);
+    detections = Array.isArray(detections) ? detections : [detections];
+
+    let targetLang = lang1;
+    if (detections.length && detections[0].language === lang1) {
+        targetLang = lang2;
+    }
+
+    const [translation] = await translate.translate(text, targetLang);
+    return translation;
+}
+
+const formatDesc = (text, translation) => {
+    return `${text}
+<br>${TL_LABEL}
+<br>${translation}`;
+}
+
+try {
+    main();
+} catch (error) {
+    core.error(error);
+    core.setFailed(error.message);
+}
